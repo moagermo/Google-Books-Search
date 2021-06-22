b33afd06eb5a0e6c9d2306bdfb571b6ab01e0aef
Mo Ager <moagermo@gmail.com>
2021-06-21 20:46:38 -0400

    Pages

--- README.old.md
+++ README.old.md
@@ -1,1 +1,3 @@
 # Google-Books-Search
+
+licenses - flaticon
--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home'
+import Saved from './Pages/Saved';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+function App()
+{
+  return(
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/saved" component={Saved}/>
+        </Switch>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
